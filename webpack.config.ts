448cf069e571217fe61dc370fc161352433bef84
greyambler <greyambler@rambler.ru>
2023-12-06 11:34:58 +0300

    Архитектура. Начинаем внедрять
    Архитектура. Начинаем внедрять

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,20 +1,22 @@
-import path from 'path';
-import webpack from 'webpack';
-import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
+import webpack from 'webpack'
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
+import path from "path";
 
 export default (env: BuildEnv) => {
 
    const paths: BuildPaths = {
       entry: path.resolve(__dirname, 'src', 'index.tsx'), // - стартовая точка приложения
       build: path.resolve(__dirname, 'build'), // - где будет приложение
-      html: path.resolve(__dirname, "public", "index.html") // - файл используется как шаблон 
+      html: path.resolve(__dirname, "public", "index.html"), // - файл используется как шаблон 
+      src: path.resolve(__dirname, 'src'),
    }
 
    const mode = env.mode || 'development';
-   const isDev = mode === 'development';
    const PORT = env.port || 3000;
 
+   const isDev = mode === 'development';
+   
    const config: webpack.Configuration = buildWebpackConfig({
       mode,
       paths,
