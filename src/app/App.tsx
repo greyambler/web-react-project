33248c5a08fac1bb30d4c0c6dc79a400e9d2f6af
greyambler <greyambler@rambler.ru>
2023-12-06 18:32:19 +0300

    import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,6 @@
-import React, { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 
