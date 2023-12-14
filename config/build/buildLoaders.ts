cac3b65ce1059061f4ed7844e1f5c669eab6a8e3
greyambler <greyambler@rambler.ru>
2023-12-14 13:35:16 +0300

    i18 next 2

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -21,7 +21,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
                [
                   "i18next-extract",
                   {
-                     locales: ['ru','en'],
+                     locales: ['ru', 'en'],
                      keyAsDefaultValue: true
                   }
                ]
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
     className?: string;
 }
 
 export const Navbar = ({className}: NavbarProps) => {
+   const {t} = useTranslation('navbar');
+
     return (
         <div className={classNames (cls.navbar, {}, [className])}>
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
-                    Главная
+                    {t('Главная')}
                 </AppLink>
                 <AppLink theme={AppLinkTheme.RED} to={'/about'}>
-                    О сайте
+                {t('О сайте')}
+                    
                 </AppLink>
             </div>
         </div>
