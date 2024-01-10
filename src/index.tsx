af83b614dc78cc06fb8899ff4899befd29617a0c
greyambler <greyambler@rambler.ru>
2024-01-10 10:43:44 +0300

    Обработка React ошибок

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./app/providers/ThemeProvider";
+import { ErrorBoundary } from "./app/providers/ErrorBoundary";
 
 import "./shared/config/i18n/i18n";
 
@@ -11,9 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
    <BrowserRouter>
       <React.StrictMode>
-         <ThemeProvider>
-            <App />
-         </ThemeProvider>
+         <ErrorBoundary>
+            <ThemeProvider>
+               <App />
+            </ThemeProvider>
+         </ErrorBoundary>
       </React.StrictMode>
    </BrowserRouter>,
 );
