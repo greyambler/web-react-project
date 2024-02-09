14d1f62aeb0368161a0b7934179d1cd4071ec51c
greyambler <greyambler@rambler.ru>
2024-02-09 14:05:28 +0300

    28 Сайдбар.
    28 Сайдбар. Состояния кнопки. UI Screenshot test report

--- src/shared/ui/Button/Button.stories.ts
+++ src/shared/ui/Button/Button.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Button, ThemeButton } from "./Button";
+import { Button, ButtonTheme, ButtonSize } from "./Button";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "app/providers/ThemeProvider";
 
@@ -17,25 +17,83 @@ export const Primary: Story = {
    },
 };
 
-
 export const Clear: Story = {
    args: {
       children: "Text",
-      theme: ThemeButton.CLEAR,
+      theme: ButtonTheme.CLEAR,
    },
 };
 
 export const Outline: Story = {
    args: {
       children: "Text",
-      theme: ThemeButton.OUTLINE,
+      theme: ButtonTheme.OUTLINE,
    },
 };
+
+export const OutlineSizeL: Story = {
+   args: {
+      children: "Text",
+      theme: ButtonTheme.OUTLINE,
+      size: ButtonSize.L,
+   },
+};
+
+export const OutlineSizeXl: Story = {
+   args: {
+      children: "Text",
+      theme: ButtonTheme.OUTLINE,
+      size: ButtonSize.XL,
+   },
+};
+
+
 
 export const OutlineDark: Story = {
    args: {
       children: "Text",
-      theme: ThemeButton.OUTLINE,
+      theme: ButtonTheme.OUTLINE,
    },
 };
+
 OutlineDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const BackgroundTheme: Story = {
+   args: {
+      children: "Text",
+      theme: ButtonTheme.BACKGROUND,
+   },
+};
+
+export const BackgroundInverted: Story = {
+   args: {
+      children: "Text",
+      theme: ButtonTheme.BACKGROUND_INVERTED,
+   },
+};
+
+export const Square: Story = {
+   args: {
+      children: ">",
+      theme: ButtonTheme.BACKGROUND_INVERTED,
+      square: true,
+   },
+};
+
+export const SquareSizeL: Story = {
+   args: {
+      children: ">",
+      theme: ButtonTheme.BACKGROUND_INVERTED,
+      square: true,
+      size: ButtonSize.L,
+   },
+};
+
+export const SquareSizeXl: Story = {
+   args: {
+      children: ">",
+      theme: ButtonTheme.BACKGROUND_INVERTED,
+      square: true,
+      size: ButtonSize.XL,
+   },
+};
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,7 +3,13 @@ import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Sidebar.module.scss";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
-import { Button } from "shared/ui/Button/Button";
+import { Button, ButtonSize, ButtonTheme } from "shared/ui/Button/Button";
+import { useTranslation } from "react-i18next";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { RoutePath } from "shared/config/routeConfig/routeConfig";
+import MainIcon from "shared/assets/icons/main-20-20.svg";
+import AboutIcon from "shared/assets/icons/about-20-20.svg";
+
 
 interface SidebarProps {
    className?: string;
@@ -11,6 +17,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
    const [collapsed, setCollapsed] = useState(false);
+   const { t } = useTranslation();
 
    const onToggle = () => {
       setCollapsed(prev => !prev);
@@ -25,10 +32,41 @@ export const Sidebar = ({ className }: SidebarProps) => {
          <Button
             data-testid="sidebar-toggle"
             onClick={onToggle}
-         >{"toggle"}</Button>
+            className={cls.collapseBtn}
+            theme={ButtonTheme.BACKGROUND_INVERTED}
+            size={ButtonSize.L}
+            square
+         >
+            {collapsed ? ">" : "<"}
+         </Button>
+         <div className={cls.items}>
+            <AppLink
+               theme={AppLinkTheme.SECONDARY}
+               to={RoutePath.main}
+               className={cls.item}
+            >
+               <MainIcon className={cls.icon} />
+               <span className={cls.link}>
+                  {t("Главная")}
+               </span>
+            </AppLink>
+            <AppLink
+               theme={AppLinkTheme.SECONDARY}
+               to={RoutePath.about}
+               className={cls.item}
+            >
+               <AboutIcon className={cls.icon} />
+               <span className={cls.link}>
+                  {t("О сайте")}
+               </span>
+            </AppLink>
+         </div>
+
          <div className={cls.switchers}>
             <ThemeSwitcher />
-            <LangSwitcher className={cls.lang} />
+            <LangSwitcher
+               short={collapsed}
+               className={cls.lang} />
          </div>
       </div>
    );
