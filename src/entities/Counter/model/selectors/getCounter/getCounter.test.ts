66f8ee617cbf70dfbf50c91016adf4c1f9bf8aee
greyambler <greyambler@rambler.ru>
2024-03-18 19:20:23 +0300

    32 Кастомный Input. Окно авторизации. Lazy modal

--- src/entities/Counter/model/selectors/getCounter/getCounter.test.ts
+++ src/entities/Counter/model/selectors/getCounter/getCounter.test.ts
@@ -2,10 +2,10 @@ import { StateSchema } from "app/providers/StoreProvider";
 import { getCounter } from "./getCounter";
 
 describe("getCounter", () => {
-   test("should return counter value", () => {
+   test("should return cournter value", () => {
       const state: StateSchema = {
          counter: { value: 10 },
       };
-      expect(getCounter(state)).toEqual({ value: 10 });
+      expect(getCounter(state as StateSchema)).toEqual({ value: 10 });
    });
 });
